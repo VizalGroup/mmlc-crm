22349093869c3299d3e8a6f385cfb3c2c2b6b99a
Alejandro Villamayor <avillamayor720@gmail.com>
2023-08-07 19:44:44 -0300

    Esta Funcional el Login,  pero no esta funcional el logout, si bien redirige afuera no deja protegida la ruta visor

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import Login from './components/Login/Login'
-import Visor from './components/Visor/Visor'
+import React, { useState } from 'react';
+import { Route, Routes, Navigate } from 'react-router-dom';
+import Login from './components/Login/Login';
+import Visor from './components/Visor/Visor';
+
+const PrivateRoute = ({ element }) => {
+  // Usa un estado para rastrear si el usuario ha iniciado sesión o no.
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  // Función para verificar si el usuario ha iniciado sesión o no (puedes implementar tu lógica aquí).
+  const checkLoggedIn = () => {
+    // Para este ejemplo, asumiremos que el usuario ha iniciado sesión si hay un nombre de usuario en el almacenamiento local.
+    return !!localStorage.getItem('username');
+  };
+
+  // Comprueba si el usuario ha iniciado sesión o no.
+  const loggedIn = checkLoggedIn();
+
+  return loggedIn ? element : <Navigate to="/" />;
+};
 
 function App() {
   return (
     <div>
       <Routes>
-        <Route path='/' element={<Login/>}/>
-        <Route path='/visor' element={<Visor/>}/>
+        <Route path="/" element={<Login />} />
+        <Route path="/visor" element={<PrivateRoute element={<Visor />} />} />
       </Routes>
-      
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
